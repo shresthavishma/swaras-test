d1f5c018d4d70a633b2c650916e2ac2aec628fb1
Vishma Shrestha <133684676+shresthavishma@users.noreply.github.com>
2025-12-30 23:48:52 +0545

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+import random
+
+app = Flask(__name__)
+@app.route("/", methods=["GET", "POST"])
+
+def home():
+    result = ""
+
+    if request.method == "POST":
+        length = int(request.form["length"])
+        swars = ['s', 'R','g', 'm', 'p', 'dha', 'n', 'saa']
+        result = ' '.join(random.sample(swars,length))
+
+    return render_template("index.html", result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
